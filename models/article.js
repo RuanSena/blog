bca351428221589afdd9f6c40f22060e0d56ce6a
RuanSena <ruansenadev@gmail.com>
2020-06-01 11:57:37 -0300

    view count

--- models/article.js
+++ models/article.js
@@ -9,6 +9,12 @@ moment.tz.setDefault('America/Bahia')
 moment.locale('pt-br')
 var Schema = mongoose.Schema
 
+const viewSchema = new Schema({
+    ip: {type: String, unique: true},
+    session: {type: String},
+    dates: [{type: Date, default: Date.now}]
+})
+
 const articleSchema = new Schema({
     title: {type: String, maxlength: 80, required: true},
     date: {type: Date, default: Date.now},
@@ -17,8 +23,9 @@ const articleSchema = new Schema({
     reference: {type: String},
     category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
     slug:{type: String, required: true, unique: true},
-    content: {type: String, required: true}
-})
+    content: {type: String, required: true},
+    views: [viewSchema]
+}, {typePojoToMixed: false})
 
 articleSchema.pre('validate', function(next) {
     this.slug = slugify(this.title, {remove: /[.]/g})
--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,8 @@ var async = require('async')
 var Article = require('../models/article')
 var Account = require('../models/account')
 var { param, validationResult } = require('express-validator')
+var moment = require('moment-timezone')
+moment.tz.setDefault('America/Bahia')
 
 router.get('/', function (req, res, next) {
     async.parallel({
@@ -31,7 +33,6 @@ router.get('/', function (req, res, next) {
         }
     }, (err, results) => {
         if (err) { return next(err) }
-        console.log(results.archives)
         res.render('home', { title: 'blog', articles: results.articles, author: results.owner, archives: results.archives})
     })
 });
@@ -44,10 +45,30 @@ router.get('/:slug', [
             return next()
         }
         Article.findOne({ slug: req.params.slug })
-            .lean({ virtuals: true })
             .exec((err, article) => {
                 if (err) { return next() }
-                res.render('post', { title: article.title, article })
+                let ip = req.ip || req.connection.remoteAddress
+                for(let view of article.views) {
+                    if(view.ip === ip) {
+                        if(req.sessionID === view.session) {
+                            // ip in same session don't save
+                            return res.render('post', { title: article.title, article })
+                        } else {
+                            // count view
+                            view.session = req.sessionID
+                            view.dates.push(Date.now())
+                            ip = false;
+                            break;
+                        }
+                    }
+                }
+                if(ip) {
+                    article.views.push({ip, session: req.sessionID, dates: [Date.now()]})
+                }
+                article.markModified('views')
+                article.save(function(err, article) {
+                    res.render('post', { title: article.title, article })
+                })
             })
     }
 ])
